6ce8ce5103f17e79d6668e5693088514b6b34a98
wudsh1 <wudsh1@users.noreply.github.com>
2026-06-25 21:37:55 +0800

    升级 AGP 8.7 + Kotlin 2.0 兼容 Gradle 9.x

--- xiancaibao-apk/app/build.gradle.kts
+++ xiancaibao-apk/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.webkit:webkit:1.9.0")
+    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.webkit:webkit:1.12.0")
 }
